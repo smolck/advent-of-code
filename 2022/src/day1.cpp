5f50819cb53a91e2a06b8c41654d89690cdbd9af
smolck <46855713+smolck@users.noreply.github.com>
2022-12-01 21:38:17 -0600

    day 1 solution (2022)

---
+++ 2022/src/day1.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+#include "config.h"
+
+#define MAX(x, y) ((x > y) ? (x) : (y))
+
+int answerPartOne(const std::string& input) {
+    std::stringstream ss(input);
+
+    int max = 0;
+    int current = 0;
+
+    for (std::string line; std::getline(ss, line); ) {
+        if (line.empty()) {
+            // End of elf so do the stuff
+            max = MAX(max, current);
+            current = 0;
+        } else {
+            int calories = std::stoi(line);
+            current += calories;
+        }
+    }
+
+    // loop ends without doing this for the last elf, so do it now
+    max = MAX(max, current);
+
+    return max;
+}
+
+int answerPartTwo(const std::string& input) {
+    std::stringstream ss(input);
+
+    std::vector<int> stuff;
+    int current = 0;
+
+    for (std::string line; std::getline(ss, line); ) {
+        if (line.empty()) {
+            // End of elf so do the stuff
+            stuff.push_back(current);
+            current = 0;
+        } else {
+            int calories = std::stoi(line);
+            current += calories;
+        }
+    }
+
+    // loop ends without doing this for the last elf, so do it now
+    stuff.push_back(current);
+
+    std::sort(stuff.rbegin(), stuff.rend());
+
+    return stuff[0] + stuff[1] + stuff[2];
+}
+
+int main() {
+    std::cout << "Example answer part one: " << answerPartOne(DAY_ONE_EXAMPLE_INPUT) << "\n";
+    std::cout << "Example answer part two: " << answerPartTwo(DAY_ONE_EXAMPLE_INPUT) << "\n";
+
+    std::cout << "Part 1 answer: " << answerPartOne(DAY_ONE_INPUT) << "\n";
+    std::cout << "Part 2 answer: " << answerPartTwo(DAY_ONE_INPUT) << "\n";
+}
